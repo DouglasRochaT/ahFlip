33d720b32c371f4d801e83a6a39f7b239538c04e
douglas <douglastuchtenhagen@gmail.com>
2021-02-20 23:49:18 -0300

    getAll Auctions

--- src/auctions/auctions.service.ts
+++ src/auctions/auctions.service.ts
@@ -10,13 +10,27 @@ export class AuctionsService {
   private cache = cacheManager.caching({store: 'memory', max: 100, ttl: 30});
   private client = new Client("aad15ae1-a30e-4ced-95a2-6e10af03bc7a", this.cache);
 
-  getAll() :AuctionClass[]{
-    let auctionsPage = this.client.skyblock.auctions.page().then(function(result :AuctionsListClass){
-      
+  async getAll() :Promise<AuctionClass[]>{
+    let auctions: AuctionClass[];
+    let numberOfPages :number;
+    let pageRequests :Promise<AuctionsListClass>[] = new Array();
+
+    await this.client.skyblock.auctions.page(0).then(function(result :AuctionsListClass){
+      auctions = result.auctions;
+      numberOfPages = result.totalPages;
     });
 
+    for (let pageIndex = 1; pageIndex < 4; pageIndex++) {
+      pageRequests.push(this.client.skyblock.auctions.page(pageIndex));
+    }
 
-    return auctionsPage.auctions; //Retorno Temporário
+    await Promise.all(pageRequests).then(function(result :AuctionsListClass[]){
+      result.forEach(function(auctionList){
+        auctions.concat(auctionList.auctions);
+      });
+    })
+    
+    return auctions;
   }
 
   getByPage(page: number): AuctionsListClass{
